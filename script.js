edce7a2fe5f261ed1895156011f0653f84f18520
clipaclip <clipaclip321@gmail.com>
2024-09-04 22:59:34 +0530

    Add files via upload
    aaaaa

--- script.js
+++ script.js
@@ -6,10 +6,12 @@ document.addEventListener('DOMContentLoaded', () => {
         
          { name: 'tommy', type: 'street puppy', description: ' friendly street puppy looking for a loving home. This sweet pup is full of curiosity and affection, ready to bring joy to a caring family.', image: 'https://th.bing.com/th/id/OIP.SOZ4PQWOi6dia0Ov09fJ8wAAAA?w=274&h=196&c=7&r=0&o=5&pid=1.7' },
         
-         { name: 'Shelby', type: 'Dog', description: 'A gentle pet turtle looking for a new home. This calm companion loves basking and exploring, perfect for adding a peaceful touch to your space.', image: 'https://th.bing.com/th/id/OIP.2y3OK9WuFlh9BPffc9oNVAHaEK?w=306&h=180&c=7&r=0&o=5&pid=1.7' },
+         { name: 'Shelby', type: 'turtle', description: 'A gentle pet turtle looking for a new home. This calm companion loves basking and exploring, perfect for adding a peaceful touch to your space.', image: 'https://th.bing.com/th/id/OIP.2y3OK9WuFlh9BPffc9oNVAHaEK?w=306&h=180&c=7&r=0&o=5&pid=1.7' },
         
         
          { name: 'Daisy', type: 'Rescued Dog', description: 'Mumbai Adoption center ,Daisy a 2.5 months old baby girl ,Vaccinated,              Location Mudh island Contact  +9189760', image: 'https://th.bing.com/th/id/OIP.gRh_3UjYLtd6ke3dgPuDYQHaFj?w=166&h=185&c=7&r=0&o=5&pid=1.7' },
+        
+         { name: 'upload', type: '', description: '', image: 'https://th.bing.com/th/id/OIP.UKZ0CHwOKft5l-KtM9u7JAAAAA?rs=1&pid=ImgDetMain' },
         // Add more animals as needed
     ];
 
